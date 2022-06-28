5a2d5eadede749f5a9be4d62c96bd38e0fc098f6
s1n7ax <srineshnisala@gmail.com>
2022-06-28 22:44:36 +0530

    chore: organize imports

--- src/srinesh/algorithms/q2_simple_array_sum/Main.java
+++ src/srinesh/algorithms/q2_simple_array_sum/Main.java
@@ -1,14 +1,12 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
 
 class Result {
 
@@ -33,8 +31,8 @@ public class Solution {
         int arCount = Integer.parseInt(bufferedReader.readLine().trim());
 
         List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-            .map(Integer::parseInt)
-            .collect(toList());
+                .map(Integer::parseInt)
+                .collect(toList());
 
         int result = Result.simpleArraySum(ar);
 
