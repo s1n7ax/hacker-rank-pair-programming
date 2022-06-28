8918f3069f9d397fadbec63fccb77c8d04bc0dc9
s1n7ax <srineshnisala@gmail.com>
2022-06-28 14:04:31 +0530

    chore: add test runnable files

---
+++ src/srinesh/q1_name_of_the_qustion.java
@@ -0,0 +1,11 @@
+package srinesh;
+
+/**
+ * q1_name_of_the_qustion
+ */
+public class q1_name_of_the_qustion {
+
+    public static void main(String[] args) {
+        System.out.println("testing");
+    }
+}
